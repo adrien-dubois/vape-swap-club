13489cfdba9a23a0b888bd07254286e977b315a9
adrien-dubois <mcsnoos@gmail.com>
2021-10-15 00:02:50 +0200

    [WIP] Brand model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,7 +2,72 @@
 
 namespace App\Models;
 
+use App\Utils\Database;
+
 class Brand extends CoreModel{
 
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var int
+     */
+    private $footer_order;
+    
 
+    public static function find($brandId)
+    {
+        $pdo = Database::getPDO();
+
+        $sql = '
+            SELECT *
+            FROM `brand`
+            WHERE id = ' . $brandId;
+
+        $pdoStatement = $pdo->query($sql);
+        $brand = $pdoStatement->fetchObject('App\Models\Brand');
+
+        return $brand;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of footer_order
+     */ 
+    public function getFooter_order()
+    {
+        return $this->footer_order;
+    }
+
+    /**
+     * Set the value of footer_order
+     *
+     * @return  self
+     */ 
+    public function setFooter_order($footer_order)
+    {
+        $this->footer_order = $footer_order;
+
+        return $this;
+    }
 }
