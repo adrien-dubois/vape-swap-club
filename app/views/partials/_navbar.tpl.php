06195c1ca1320c5b869adaa7298e571a5d8ec28e
adrien-dubois <mcsnoos@gmail.com>
2021-10-19 14:14:14 +0200

    [FINISH] Product list page / [WIP] Single product page

--- app/views/partials/_navbar.tpl.php
+++ app/views/partials/_navbar.tpl.php
@@ -3,12 +3,16 @@
         <a href="/" class="logo-link">
             <img src="<?= $assetsBaseUri; ?>images/logo.png" alt="" class="logo-image">
         </a>
+
+        <!-- LINKS NAVIGATION -->
         <div class="links">
             <a href="<?= $this->router->generate('main-home'); ?>" class="nav-links <?php if ($currentPage === 'main/home') : echo 'act'; endif; ?>">Accueil</a>
             <a href="<?= $this->router->generate('product-list') ?>" class="nav-links <?php if($currentPage === 'product/list') : echo 'act'; endif; ?>">Annonces</a>
             <a href="#" class="nav-links">Catégories</a>
             <a href="#" class="nav-links">Contact</a>
         </div>
+
+        <!-- PERSONNAL MENU AFTER CONNECT -->
         <aside class="menu">
             <?php if(isset($_SESSION['userId'])) :  ?>
             <?php 
@@ -24,7 +28,7 @@
                     <ul>
                         <li><a href="#"><i class="fas fa-shopping-cart"></i>Mon Panier</a></li>
                         <li><a href="#"><i class="far fa-user-circle"></i>Mon Profil</a></li>
-                        <li><a href="#"><i class="far fa-edit"></i>Éditer Profil</a></li>
+                        <li><a href="#"><i class="far fa-edit"></i>Vendre du matos</a></li>
                         <li><a href="#"><i class="far fa-envelope"></i>Messagerie</a></li>
                         <li><a href="<?= $router->generate('main-logout') ?>"><i class="fas fa-sign-out-alt"></i>Se déconnecter</a></li>
                     </ul>
--- app/views/product/list.tpl.php
+++ app/views/product/list.tpl.php
@@ -1,13 +1,11 @@
-<main class="main">
-
 <!-- LITTLE HERO SECTION -->
     <div class="header">
         <div class="container">
-            <div class="row">
+            <div class="row row-2">
                 <div class="column-2">
                     <span>Last hotest news</span>
                     <h1>Annonces</h1>
-                    <p>Voici nos dernières annonces de produits High-End vérifiés, que ce soit du RTA MTL au Dripper pour Cloud Chasers, en passant par des mods mécas custom ou aux boxs BF faites à la mano en bois stab. <br> Tout le monde peut y trouver chaussure à son pied ou même changer de style, qu'importe tant que c'est du lourd !!</p>
+                    <p>Voici nos dernières annonces de produits High-End vérifiés, que ce soit du RTA MTL au Dripper pour Cloud Chasers, en passant par des mods mécas custom ou aux boxs BF faites à la mano en bois stab. <br> Tout le monde peut y trouver chaussure à son pied ou même changer de style, qu'importe tant que c'est votre vape et que c'est du lourd !!</p>
                     <a href="#" class="btn-primary">Explorer &#8594; </a>
                 </div>
                 <div class="column-2">
@@ -20,35 +18,42 @@
     <!-- PRODUCTS -->
 
     <div class="container">
-        <h2>Nos produits disponibles</h2>
+        <div class="row">
+            <h2 class="list-title">Nos produits disponibles</h2>
+        </div>
         <div class="row">
             <div class="column-4">
                 <img src="<?= $assetsBaseUri; ?>uploads/had.png">
                 <h4>Hadaly Gold RDA</h4>
+                <p class="brand">Psyclone mods</p>
                 <div class="rating">
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
                     <i class="fa fa-star-o"></i>
                 </div>
                 <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
             </div>
             <div class="column-4">
                 <img src="<?= $assetsBaseUri; ?>uploads/d5.png">
                 <h4>C2MNT RDA</h4>
+                <p class="brand">District F5ve</p>
                 <div class="rating">
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
-                    <i class="fa fa-star-half"></i>
+                    <i class="fas fa-star-half-alt"></i>
                 </div>
                 <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
             </div>
             <div class="column-4">
                 <img src="<?= $assetsBaseUri; ?>uploads/origen.png">
                 <h4>Origen V2 MK-II</h4>
+                <p class="brand">Norbert</p>
                 <div class="rating">
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
@@ -57,10 +62,12 @@
                     <i class="fa fa-star-o"></i>
                 </div>
                 <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
             </div>
             <div class="column-4">
-                <img src="<?= $assetsBaseUri; ?>uploads/go.png">
-                <h4>Goon 25 RDA</h4>
+                <img src="<?= $assetsBaseUri; ?>uploads/reckoning.jpg">
+                <h4>Reckoning RDA</h4>
+                <p class="brand">Immortal Modz</p>
                 <div class="rating">
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
@@ -69,8 +76,74 @@
                     <i class="fa fa-star-o"></i>
                 </div>
                 <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
             </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="column-4">
+                <img src="<?= $assetsBaseUri; ?>uploads/bolt.jpg">
+                <h4>Bolt Mod</h4>
+                <p class="brand">Comp Lyfe</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
+            </div>
+            <div class="column-4">
+                <img src="<?= $assetsBaseUri; ?>uploads/spade.png">
+                <h4>Spade Box BF</h4>
+                <p class="brand">Vicious Ant</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
+            </div>
+            <div class="column-4">
+                <img src="<?= $assetsBaseUri; ?>uploads/cl-stark.jpeg">
+                <h4>Stark Mod</h4>
+                <p class="brand">CompLyfe </p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
+            </div>
+            <div class="column-4">
+                <img src="<?= $assetsBaseUri; ?>uploads/cobra-slam.jpg">
+                <h4>Cobra Slam Hagerman Edition</h4>
+                <p class="brand">Purge Mods</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>40.00 €</p>
+                <a href="#" class="btn-secondary">VOIR</a>
+            </div>
+        </div>
 
-</main>
+        <div class="page-btn">
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>&#8594;</span>
+        </div>
+
+    </div>
