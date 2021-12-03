2027da1bb928023983cb8080704e298cf3fbac97
adrien-dubois <mcsnoos@gmail.com>
2021-12-03 23:31:30 +0100

    [WIP]CSS Layout BO

--- app/views/backoffice/home.tpl.php
+++ app/views/backoffice/home.tpl.php
@@ -1,83 +1,83 @@
-<div class="sidebar">
-    <div class="sidebar-brand">
-        <h2><span class="fas fa-tools"></span>Administration</h2>
-    </div>
+<div class="glob">
 
-    <div class="sidebar-menu">
-        <ul>
-            <li><a href="#"><span class="fa fa-home"></span><span>Accueil</span></a></li>
-            <li><a href="#"><span class="fa fa-user-o" ></span><span>Utilisateurs</span></a></li>
-            <li><a href="#"><span class="fas fa-user-tie" ></span><span>Demande vendeurs</span></a></li>
-            <li><a href="#"><span class="fas fa-shopping-cart" ></span><span>Articles</span></a></li>
-            <li><a href="#"><span class="fas fa-tags" ></span><span>Catégories </span></a></li>
-        </ul>
-    </div>
-
-</div>
-
-<div class="bo-content">
-    <header>
-        <p><label for="menu"><span class="fa fa-bars"></span></label></p>
-    </header>
-
-    <main>
-        <div class="bo-cards">
-
-            <!-- CARD -->
-            <div class="card-single">
-                <div>
-                    <h2>50</h2>
-                    <small>Utilisateurs</small>
-                </div>
-                <div>
-                    <span class="fa fa-user-o"></span>
-                </div>
-            </div>
-
-            <!-- CARD -->
-            <div class="card-single">
-                <div>
-                    <h2>50</h2>
-                    <small>Vendeurs</small>
-                </div>
-                <div>
-                    <span class="fas fa-user-tie"></span>
-                </div>
-            </div>
-
-            <!-- CARD -->
-            <div class="card-single">
-                <div>
-                    <h2>50</h2>
-                    <small>Articles</small>
-                </div>
-                <div>
-                    <span class="fas fa-shopping-cart"></span>
-                </div>
-            </div>
-
-            <!-- CARD -->
-            <div class="card-single">
-                <div>
-                    <h2>50</h2>
-                    <small>Catégories</small>
-                </div>
-                <div>
-                    <span class="fas fa-tags"></span>
-                </div>
-            </div>
-
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="fas fa-tools"></span>Administration</h2>
         </div>
- 
-        <div class="composant">
-            <div class="ventes">
-                <div class="case">
-                    <div class="header-case">
-                        <h2>Listes produits</h2>
-                        <button class="bo-button">Voir plus<span class="fa fa-arrow-right"></span></button>
+
+        <div class="sidebar-menu">
+            <ul>
+                <li><a href="#" class="active" ><span class="fa fa-home"></span><span>Accueil</span></a></li>
+                <li><a href="#"><span class="fa fa-user-o" ></span><span>Utilisateurs</span></a></li>
+                <li><a href="#"><span class="fas fa-user-tie" ></span><span>Demande vendeurs</span></a></li>
+                <li><a href="#"><span class="fas fa-shopping-cart" ></span><span>Articles</span></a></li>
+                <li><a href="#"><span class="fas fa-tags" ></span><span>Catégories </span></a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="bo-content">
+
+        <main>
+            <div class="bo-cards">
+
+                <!-- CARD -->
+                <div class="card-single">
+                    <div>
+                        <h2>50</h2>
+                        <small>Utilisateurs</small>
+                    </div>
+                    <div>
+                        <span class="fa fa-user-o"></span>
+                    </div>
+                </div>
+
+                <!-- CARD -->
+                <div class="card-single">
+                    <div>
+                        <h2>50</h2>
+                        <small>Vendeurs</small>
+                    </div>
+                    <div>
+                        <span class="fas fa-user-tie"></span>
+                    </div>
+                </div>
+
+                <!-- CARD -->
+                <div class="card-single">
+                    <div>
+                        <h2>50</h2>
+                        <small>Articles</small>
+                    </div>
+                    <div>
+                        <span class="fas fa-shopping-cart"></span>
+                    </div>
+                </div>
+
+                <!-- CARD -->
+                <div class="card-single">
+                    <div>
+                        <h2>50</h2>
+                        <small>Catégories</small>
+                    </div>
+                    <div>
+                        <span class="fas fa-tags"></span>
+                    </div>
+                </div>
+
+            </div>
+    
+            <div class="composant">
+                <div class="ventes">
+                    <div class="case">
+                        <div class="header-case">
+                            <h2>Listes produits</h2>
+                            <button class="bo-button">Voir plus<span class="fa fa-arrow-right"></span></button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
+        </main>
+    </div>
 </div>
